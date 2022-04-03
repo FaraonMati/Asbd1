16157570e3ae03b5113445299677eda52bd64d20
FaraonMati <101049372+FaraonMati@users.noreply.github.com>
2022-04-03 06:22:14 +0200

    Add files via upload
    4th task

--- Program.cs
+++ Program.cs
@@ -14,26 +14,62 @@ namespace Crawler
         public async static Task Main(string[] args)
         {
             //var websiteUr1 = args[0];
-            string websiteUr1 = "https://pja.edu.pl/";
+            //string websiteUr1 = "https://pja.edu.pl/";
 
-            var list = new List<string>();
-            var dictionary = new Dictionary<string, string>();
-            var set = new HashSet<string>();
-
+            if (args.Length < 1)
+            {
+                throw new ArgumentNullException();
+            }
+            var websiteUr = args[0];
             var httpClient = new HttpClient();
 
-            var response = await httpClient.GetAsync(websiteUr1);
-            var content = await response.Content.ReadAsStringAsync();
+            // uri
+            // https://docs.microsoft.com/pl-pl/dotnet/api/system.uri?view=net-6.0
+
+            Uri uriTestbox;
+            var iswebsiteUr = Uri.TryCreate(websiteUr, UriKind.Absolute, out uriTestbox);
+            if (!iswebsiteUr)
+            {
+                throw new ArgumentException();
+            }
+            var source = await httpClient.GetAsync(websiteUr);
+            if (!source.IsSuccessStatusCode)
+            {
+                throw new Exception("## Download site error !/ Blad pobierania strony ##");
+            }
+            httpClient.Dispose();
+            var page = await source.Content.ReadAsStringAsync();
+
+            //var list = new List<string>();
+            //var dictionary = new Dictionary<string, string>();
+
+            //var response = await httpClient.GetAsync(websiteUr1);
+            //var content = await response.Content.ReadAsStringAsync();
 
 
-            var regex = new Regex(@"[await-zA-Z0-9_.+-]+@[a-zA-Z0-9]+\.[a-zA-Z0-9-.=]+");
-            var matchCollection = regex.Matches(content);
+           // var regex = new Regex(@"[await-zA-Z0-9_.+-]+@[a-zA-Z0-9]+\.[a-zA-Z0-9-.=]+");
+
+
+            var regex = new Regex(@"[a-zA-Z0-9_]+@([a-zA-Z]*\.)*[a-zA-Z]*");
+            var matchCollection = regex.Matches(page);
+
+
+            var set = new HashSet<string>();
+            if (matchCollection.Count < 1)
+            {
+                Console.WriteLine(" ## No mails found! ##");
+            }
 
             foreach (var item in matchCollection)
 
             {
 
                 Console.WriteLine(item);
+                if (!set.Contains(item.ToString()))
+                {
+                    Console.WriteLine(item);
+                    set.Add(item.ToString());
+                }
             }
         }
     }
